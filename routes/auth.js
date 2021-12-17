1b4e7b94a90b4ea1bb08243e06bc55ea249903a9
Siddharth Sharma <siddharthsharma653@gmail.com>
2021-12-17 19:17:54 +0530

    9th commit

--- routes/auth.js
+++ routes/auth.js
@@ -2,14 +2,25 @@ const express = require('express');
 const { model } = require('mongoose');
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
-// Create a User using: POST "/api/auth". Dosen't require Auth
-router.post('/', (req,res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+// Create a User using: POST "/api/auth/createsuser". No login required
+router.post('/createsuser', [
+    body('name','Enter a valid name').isLength({ min: 3 }),
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
+],(req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name ,
+        email: req.body.email,
+        password: req.body.password,
+      }).then(user => res.json(user));
 
+   
 })
 
 module.exports = router;
