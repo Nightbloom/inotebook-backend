5713e9c0bfb953c91947f5b02e0fa1f56878527a
Siddharth Sharma <siddharthsharma653@gmail.com>
2021-12-17 20:35:20 +0530

    10th commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { model } = require('mongoose');
 const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
@@ -9,17 +8,33 @@ router.post('/createsuser', [
     body('name','Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
-],(req,res)=>{
+], async (req,res)=>{
+
+    //If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+
+    //Check whether the user exists alredy with this email
+try{
+
+    
+    let user = await User.findOne({email: req.body.email});
+    if(user){
+        return res.status(400).json({error: "Sorry a user with this email already exists"})
+    }
+    user = await User.create({
         name: req.body.name ,
         email: req.body.email,
         password: req.body.password,
-      }).then(user => res.json(user));
+    })
 
+    res.json({user});
+} catch(error){
+    console.error(error.message);
+    res.status(500).send("Some error occured");
+}
    
 })
 
