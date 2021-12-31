e6602a24cae844e33459a86bc793c0793957c8bc
Siddharth Sharma <siddharthsharma653@gmail.com>
2021-12-31 17:12:43 +0530

    17th commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const connectToMongo = require('./db');
 const express = require('express')
+var cors = require('cors')
 
 connectToMongo();
 
@@ -7,6 +8,7 @@ connectToMongo();
 const app = express()
 const port = 5000;
 
+app.use(cors())
 app.use(express.json());
 
 //Avaliable Routes
@@ -14,6 +16,6 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`iNotebook backend listening at http://localhost:${port}`)
 })
 
